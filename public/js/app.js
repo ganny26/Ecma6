66fbbe3a2794709939f37ac5b5ac69a7be227d09
selvaganeshr <selvaganeshrajam@gmail.com>
2017-11-28 23:45:41 +0530

    history added

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,45 @@
 console.log('Loaded app js');
-let command = document.getElementById('command');
+
+let historyButton = document.getElementById('btn_get_history');
+
+historyButton.addEventListener('click', () => {
+    console.log('clicked');
+    run('@history');
+})
 
 
-document.addEventListener('keypress', (event) => {
-    const keyName = event.key;
-    console.log("keyname", keyName);
-});
+/**
+ * method to set history when commands entered in console
+ * @param {*} history 
+ */
+
+let setHistory = (history) => {
+    if (typeof JSON == 'undefined') return;
+
+    try {
+        sessionStorage.setItem('history', JSON.stringifyContent(history));
+    } catch (e) {}
+}
+
+
+/**
+ * method to show history of console
+ */
+let showHistory = () => {
+    var h = getHistory();
+    h.shift();
+    return h.join("\n");
+}
+
+
+/**
+ * method to get history of console from browser session
+ */
+let getHistory = () => {
+    var history = [''];
+    if (typeof JSON == 'undefined') return history;
+    try {
+        history = JSON.parse(sessionStorage.getItem('history') || '[""]');
+    } catch (e) {}
+    return history;
+}
